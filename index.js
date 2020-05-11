a02bef5e901ba72783d809d51a1890c4664c2e13
Mohit <mohit.dhiman.sarkaghat@gmail.com>
2020-05-11 17:57:15 +0530

    Twentyfourcommit

--- index.js
+++ index.js
@@ -1,57 +1,92 @@
 function fetchJSON(url) {
-    return new Promise((resolve, reject) => {
-      const xhr = new XMLHttpRequest();
-      xhr.open('GET', url);
-      xhr.responseType = 'json';
-      xhr.onload = () => {
-        if (xhr.status >= 200 && xhr.status <= 299) {
-          resolve(xhr.response);
-        } else {
-          reject(new Error(`Network error: ${xhr.status} - ${xhr.statusText}`));
-        }
-      };
-      xhr.onerror = () => reject(new Error('Network request failed'));
-      xhr.send();
-    });
-  }
-
-  function createAndAppend(name, parent, options = {}) {
-    const elem = document.createElement(name);
-    parent.appendChild(elem);
-    Object.entries(options).forEach(([key, value]) => {
-      if (key === 'text') {
-        elem.textContent = value;
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.responseType = 'json';
+    xhr.onload = () => {
+      if (xhr.status >= 200 && xhr.status <= 299) {
+        resolve(xhr.response);
       } else {
-        elem.setAttribute(key, value);
+        reject(new Error(`Network error: ${xhr.status} - ${xhr.statusText}`));
       }
-    });
-    return elem;
-  }
+    };
+    xhr.onerror = () => reject(new Error('Network request failed'));
+    xhr.send();
+  });
+}
 
-  function buildTableRow(title, parent, content) {
-    const row = createAndAppend(`tr`, parent);
-    createAndAppend(`th`, row, { text: `${title}:` });
-    createAndAppend(`td`, row, { text: `${content}` });
-    return row;
-  }
+function createAndAppend(name, parent, options = {}) {
+  const elem = document.createElement(name);
+  parent.appendChild(elem);
+  Object.entries(options).forEach(([key, value]) => {
+    if (key === 'text') {
+      elem.textContent = value;
+    } else {
+      elem.setAttribute(key, value);
+    }
+  });
+  return elem;
+}
 
-  function renderRepoDetails(repo, parent) {
-    const table = createAndAppend(`table`, parent);
+function buildTableRow(title, parent, content) {
+  const row = createAndAppend(`tr`, parent);
+  createAndAppend(`th`, row, { text: `${title}:` });
+  createAndAppend(`td`, row, { text: `${content}` });
+  return row;
+}
 
-    const firstRow = buildTableRow(`Repository`, table, ``);
-    createAndAppend(`a`, firstRow.lastChild, {
-      href: repo.html_url,
-      target: `_blank`,
+function renderRepoDetails(repo, parent) {
+  const table = createAndAppend(`table`, parent);
+
+  const firstRow = buildTableRow(`Repository`, table, ``);
+  createAndAppend(`a`, firstRow.lastChild, {
+    href: repo.html_url,
+    target: `_blank`,
+    text: repo.name,
+  });
+
+  const descriptionRow = buildTableRow(`Description`, table, ``);
+  const description = repo.description
+    ? { text: repo.description }
+    : { text: 'No Description', class: 'alert-no-description' };
+  createAndAppend(`Description`, descriptionRow.lastChild, description);
+
+  buildTableRow(`Forks`, table, repo.forks);
+
+  const date = new Date(repo.updated_at);
+  buildTableRow(`Updated`, table, date.toLocaleString());
+}
+
+
+
+function selectRepo(select, mainSection, repos) {
+  const repo = repos[select.value];
+  const detailsSection = createAndAppend(`section`, mainSection, {
+    class: `details`,
+  });
+  
+  renderRepoDetails(repo, detailsSection);
+  
+}
+
+function main(url) {
+  const root = document.getElementById('root');
+
+  const header = createAndAppend(`header`, root);
+  createAndAppend(`h1`, header, { text: `HYF Repositories` });
+  const select = createAndAppend(`select`, header);
+
+  const mainSection = createAndAppend(`main`, root, {
+    class: `main-container`,
+  });
+  const promise = fetchJSON(url);
+  promise.then(repo =>
+    repos
+    .sort((a, b) => a.name.localCompare(b.name))
+    .forEach((repo, index)=>
+    createAndAppend(`option`,select,{
       text: repo.name,
-});
-const descriptionRow = buildTableRow(`Description`, table, ``);
-    const description = repo.description
-      ? { text: repo.description }
-      : { text: 'No Description', class: 'alert-no-description' };
-    createAndAppend(`Description`, descriptionRow.lastChild, description);
-
-    buildTableRow(`Forks`, table, repo.forks);
-
-    const date = new Date(repo.updated_at);
-    buildTableRow(`Updated`, table, date.toLocaleString());
-  }
+      value: index,
+    }),
+    ),
+    );
