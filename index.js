7ab19d90d27a0b99ba4d6b5aed8c188b7ff182b7
Mohit <mohit.dhiman.sarkaghat@gmail.com>
2020-05-11 15:51:58 +0530

    Twentytwoecommit

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
-function fetchJSON(url) {
-    return new Promise((resolve, reject)=>{
-        const xhr = new XMLHttpRequest();
-        xhr.open('GET',url);
-        xhr.responseType = 'json';
-        xhr.onload = () => {
-            if (xhr.status >= 200 && xhr.status <= 299){
-                resolve(xhr.response);
-            } else{
-                    reject(new Error(`Network error: ${xhr.status} - ${xhr.statusText}`));
-            }
-        };
-        xhr.onerror = () => reject(new Error('Network request failed'));
-        xhr.send();
-    })
-}
+  function fetchJSON(url){
+      return new Promise((resolve, reject)=>{
+          const xhr = new XMLHttpRequest();
+          xhr.open('GET',url);
+          xhr.responseType = 'json';
+          xhr.onload = () => {
+              if(xhr.status >= 200 && xhr.status <= 299){
+                  resolve(xhr.response);
+              } else{
+                reject(new Error(`Network error: ${xhr.status} - ${xhr.statusText}`));
+              }
+          };
+          xhr.onerror = () => reject(new Error('Network request failed'));
+          xhr.send();
+      });
+  }  
+  function createAndAppend(name, parent, options = {}){
+      const elem = document.createElement(name);
+      parent.appendChild(elem);
+      Object.entries(options).forEach(([key, value]) => {
+        if (key === 'text') {
+          elem.textContent = value;
+        } else {
+          elem.setAttribute(key, value);
+        }
+      });
+      return elem;
+    }
